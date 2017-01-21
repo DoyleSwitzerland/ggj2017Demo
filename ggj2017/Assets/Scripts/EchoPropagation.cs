8d544a779772cb2836b4e1e792d22b48b4575e4d
Gareth Yawching-Rickwood <garethrickwood@hotmail.com>
2017-01-21 11:26:32 -0400

    Deleted a whole bunch of garbage junk (can't wait to delete more!) Basic single object echo creation - press mouse to shoot a thing that makes light when it hits and obstacle.

---
+++ ggj2017/Assets/Scripts/EchoPropagation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EchoPropagation : MonoBehaviour {
+    public float lifeTime;
+
+    private Light echoLight;
+    private Rigidbody echoRigidBody;
+    private Collider echoCollider;
+
+    public Rigidbody EchoRigidBody {
+        get { return echoRigidBody; }
+    }
+
+    public Collider EchoCollider {
+        get { return echoCollider; }
+    }
+
+    // Use this for initialization
+    public void Setup(Transform parent, float echoSpeed) {
+        echoLight = gameObject.GetComponent<Light>();
+        echoRigidBody = gameObject.GetComponent<Rigidbody>();
+        echoCollider = gameObject.GetComponent<SphereCollider>();
+
+        transform.position = new Vector3(parent.position.x, parent.position.y, parent.position.z + transform.position.z);
+        EchoRigidBody.velocity = new Vector3(echoSpeed, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        lifeTime -= Time.deltaTime;
+        if (lifeTime  <= 0) {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter(Collision collision) {
+        if (!collision.gameObject.CompareTag("Player")) {
+            print("Collided with " + collision.gameObject.name);
+            echoLight.enabled = true;
+            EchoRigidBody.velocity = Vector3.zero;
+            print("setting " + EchoRigidBody + " velocity to " + EchoRigidBody.velocity);
+        }
+    }
+}
---
+++ ggj2017/Assets/Scripts/EchoSource.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EchoSource : MonoBehaviour {
+    public EchoPropagation baseEchoPropagation;
+    public float coolDown;
+    public float echoSpeed;
+
+    public float recharge;
+
+	// Use this for initialization
+	void Start () {
+        recharge = coolDown;
+    }
+
+    void Update() {
+        recharge += Time.deltaTime*100;
+        if (recharge >= coolDown && Input.GetAxisRaw("Fire1") == 1) {
+            CreateEcho();
+            recharge = 0;
+        }
+    }
+
+    void CreateEcho() {
+        EchoPropagation echoPropagation = Instantiate<EchoPropagation>(baseEchoPropagation);
+
+        echoPropagation.Setup(transform, echoSpeed);
+    }
+}
