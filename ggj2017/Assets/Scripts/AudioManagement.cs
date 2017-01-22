d939782c8c4b3fb2680d32790dd611e365693a11
Dylan S <dylstz@yahoo.ca>
2017-01-22 13:49:57 -0400

    Partially completed UI pause and death screens

---
+++ ggj2017/Assets/Scripts/AudioManagement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManagement : MonoBehaviour {
+
+    public Slider volumeSlider;
+    public AudioSource volumeAudio;
+
+    public void VolumeController()
+    {
+        volumeSlider.value = volumeAudio.volume;
+    }
+}
---
+++ ggj2017/Assets/Scripts/PauseHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseHandler : MonoBehaviour {
+
+    public GameObject Canvas;
+    public GameObject Camera;
+    bool Paused = false;
+
+    void Start()
+    {
+   //     Canvas.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            print("Pressed escape.");
+            if (Paused)
+            {
+                Time.timeScale = 1.0f;
+                Canvas.gameObject.SetActive(false);
+                Cursor.visible = false;
+           //     Screen.lockCursor = true;
+           //     Camera.GetComponent<AudioSource>().Play();
+                Paused = false;
+            }
+            else
+            {
+                Time.timeScale = 0.0f;
+                Canvas.gameObject.SetActive(true);
+                Cursor.visible = true;
+              //  Screen.lockCursor = false;
+             //   Camera.GetComponent<AudioSource>().Pause();
+                Paused = true;
+            }
+        }
+    }
+    public void Resume()
+    {
+        print("resuming");
+        Time.timeScale = 1.0f;
+        Canvas.gameObject.SetActive(false);
+        Cursor.visible = false;
+     //   Screen.lockCursor = true;
+     //   Camera.audio.Play();
+    }
+}    
+//	}
+//}
